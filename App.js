e1644214e20186890533fd00d8a0b78ffd09b2df
oles <olchyk89@gmail.com>
2018-11-18 22:31:43 +0100

    Added basic songs list template / installed redux

--- App.js
+++ App.js
@@ -1,19 +1,71 @@
 import React, { Component, Fragment } from 'react';
 import {
   View,
-  Text
+  Text,
+  Image,
+  ScrollView
 } from 'react-native';
 // import { MediaLibrary } from 'expo';
 
 import styles from './styles';
 
+import image from './image.png';
+
+const iconsPath = '/assets/icons/';
+const icons = {
+  play: require('.' + iconsPath + 'play.png')
+}
+
+class ListSong extends Component {
+  render() {
+    return(
+      <View style={[ styles.listSongsSong ]}>
+        <View style={[ styles.listSongsSongProgress ]} />
+        <View style={[ styles.listSongsSongMain ]}>
+          <View style={[ styles.listSongsSongInfo ]}>
+            <View style={[ styles.listSongsSongInfoImagecointainer ]}>
+              <Image
+                style={[ styles.listSongsSongInfoImagecointainerImage ]}
+                source={ image }
+                resizeMode="cover"
+              />
+            </View>
+            <View style={[ styles.listSongsSongInfoTitle ]}>
+              <Text style={[ styles.listSongsSongInfoTitleName ]}>
+                There is Hope
+              </Text>
+              <Text style={[ styles.listSongsSongInfoTitleLabel ]}>
+                Zoo Brazil
+              </Text>
+            </View>
+          </View>
+          <View style={[ styles.listSongsSongControls ]}>
+            <Image style={[ styles.listSongsSongControlsPlay ]} source={ icons.play } />
+          </View>
+        </View>
+      </View>
+    );
+  }
+}
+
 class List extends Component {
   render() {
     return(
-      <View style={ styles.list }>
-        <View style={ styles.listHead }>
-          <Text style={ styles.listHeadTitle }>Driving</Text>
+      <View style={[ styles.list, styles.display ]}>
+        <View style={[ styles.listHead ]}>
+          <Text style={[ styles.listHeadTitle ]}>Driving</Text>
         </View>
+        <ScrollView style={[ styles.listSongs ]}>
+          <ListSong />
+          <ListSong />
+          <ListSong />
+          <ListSong />
+          <ListSong />
+          <ListSong />
+          <ListSong />
+          <ListSong />
+          <ListSong />
+        </ScrollView>
       </View>
     );
   }
@@ -30,7 +82,6 @@ class App extends Component {
     return(
       <Fragment>
         <List />
-        <Text>1</Text>
       </Fragment>
     );
   }
--- styles.js
+++ styles.js
@@ -1,16 +1,38 @@
-import { StyleSheet } from 'react-native';
+import {
+    StyleSheet,
+    StatusBar,
+    Dimensions,
+    Platform
+} from 'react-native';
+
+const {
+    width: innerWidth,
+    height: innerHeight
+} = Dimensions.get("window");
 
 const variables = {
-
+    listSongs: {
+        padding: innerWidth / 100 * 1 // 5%
+    },
+    listSongsSong: {
+        height: 85,
+        imageHeight: 65, // %
+        infoMargin: 1.5,
+        controlsSize: 20
+    }
 }
 
 const styles = StyleSheet.create({
+    display: {
+        paddingTop: (Platform.OS !== 'ios' ? StatusBar.currentHeight : 20) + 25,
+        paddingBottom: (Platform.OS !== 'ios' ? StatusBar.currentHeight : 20) + 25
+    },
     list: {
         position: "absolute",
         top: 0,
         left: 0,
         right: 0,
-        bottom: 0
+        bottom: 0,
     },
     listHead: {
         alignItems: "center",
@@ -18,7 +40,73 @@ const styles = StyleSheet.create({
     },
     listHeadTitle: {
         color: "black",
-        fontSize: 18
+        fontSize: 24,
+        fontWeight: "600"
+    },
+    listSongs: {
+        flexDirection: "column",
+        width: "100%",
+        marginTop: 25
+    },
+    listSongsSong: {
+        height: variables.listSongsSong.height,
+        width: "100%",
+        alignItems: "center",
+        justifyContent: "center",
+        flexDirection: "row"
+    },
+    listSongsSongProgress: {
+        position: "absolute",
+        top: 0,
+        bottom: 0,
+        left: 0,
+        backgroundColor: "rgba(0, 0, 0, .05)",
+        width: "40%"
+    },
+    listSongsSongMain: {
+        width: 100 - 100 / (innerWidth / variables.listSongs.padding) + "%",
+        alignItems: "center",
+        flexDirection: "row"
+    },
+    listSongsSongInfo: {
+        alignItems: "center",
+        flexDirection: "row"
+    },
+    listSongsSongInfoTitle: {
+        flexDirection: "column"
+    },
+    listSongsSongInfoTitleName: {
+        fontWeight: "500",
+        fontSize: 17.5,
+        marginBottom: variables.listSongsSong.infoMargin
+    },
+    listSongsSongInfoTitleLabel: {
+        fontWeight: "200",
+        fontSize: 15,
+        color: "rgba(0, 0, 0, .4)",
+        marginTop: variables.listSongsSong.infoMargin
+    },
+    listSongsSongInfoImagecointainer: {
+        height: variables.listSongsSong.height / 100 * variables.listSongsSong.imageHeight,
+        width: variables.listSongsSong.height / 100 * variables.listSongsSong.imageHeight,
+        marginRight: 15
+    },
+    listSongsSongInfoImagecointainerImage: {
+        height: variables.listSongsSong.height / 100 * variables.listSongsSong.imageHeight,
+        width: variables.listSongsSong.height / 100 * variables.listSongsSong.imageHeight,
+        borderRadius: 2.5
+    },
+    listSongsSongControls: {
+        flexDirection: "row",
+        alignItems: "center",
+        position: "absolute",
+        height: variables.listSongsSong.height,
+        right: 0,
+        opacity: .75
+    },
+    listSongsSongControlsPlay: {
+        height: variables.listSongsSong.controlsSize,
+        width: variables.listSongsSong.controlsSize
     }
 });
 
